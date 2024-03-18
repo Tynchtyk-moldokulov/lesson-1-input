dbbfa9ace2dabd28b9e0018ce2b0df6306a6b3c9
Tynchtyk-moldokulov <163808865+Tynchtyk-moldokulov@users.noreply.github.com>
2024-03-18 10:46:12 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// let num = Number(prompt('введите число'))
+// if (num %2 == 0) {
+//   alert("число четное");
+// } else {
+//   alert("число не четное");
+// }
+// let num = Number (prompt('введите число'))
+// if(num => 0){
+//     alert(`это число положительное ${num}`)
+// }else if (num == 0){
+//     alert(`это число равна ${num}`)
+// }else if (num <= 0){
+//     alert(`число отриц ${num}`)
+// }
+// let a = 5338
+// let s = 38747
+// let sum = a*s
+// document.write(sum)
+for (let i = 0; i < 20; i--) {
+  document.write(i);
+}
